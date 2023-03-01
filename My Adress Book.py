af37d153408296d516148278da9b06b9ab186a96
Neo <80315475+Benzo-Fury@users.noreply.github.com>
2023-03-01 13:42:40 +1000

    fix: fixed password gen
    Signed-off-by: Neo <80315475+Benzo-Fury@users.noreply.github.com>

--- My Adress Book.py
+++ My Adress Book.py
@@ -12,14 +12,14 @@ c = connection.cursor()
 
 
 def abort_process():
-    # function for aborting the process
+    # function to abort the process
     print('Aborting process')
     time.sleep(2)
     sys.exit()
 
 
 def add_password(password):
-    # adds a password to the database
+    # function to add a password to the database
     website = input("Enter website name: ")
     if not website.endswith('.com'):
         print('Website MUST end with ".com"')
@@ -27,7 +27,7 @@ def add_password(password):
     c.execute("SELECT * FROM passwords WHERE website = ?", (website,))
     result = c.fetchone()
     if result:
-        print('That password already exists.')
+        print('That website is already in the database.')
         time.sleep(2)
         os.system('cls')
         return
@@ -43,22 +43,20 @@ def add_password(password):
 
 
 def show_passwords():
-    # lists all of the stored passwords
+    # function to list all of the stored passwords
     # TODO: change format of outputted results.
     check_password = input('Input admin password: ')
     if check_password != 'monkeys':
         print('Wrong password')
         abort_process()
-    # returning all rows to user
     os.system('cls')
     c.execute("SELECT * FROM passwords")
     rows = c.fetchall()
-    print('Password List:')
     print(rows)
 
 
 def search_passwords(search):
-    # searches for a specific password.
+    # function to search for a specific password.
     c.execute("SELECT * FROM passwords WHERE website = ? OR username = ?", (search, search))
     rows = c.fetchall()
     if not rows:
@@ -70,7 +68,7 @@ def search_passwords(search):
 
 
 def update_password(password):
-    # updates a specific password that can be searched for.
+    # function to update a specific password that can be searched for.
     enter = input('Input website name: ')
     c.execute("SELECT * FROM passwords WHERE website = ?", (enter,))
     result = c.fetchone()
@@ -88,7 +86,7 @@ def update_password(password):
 
 
 def delete_password():
-    # deletes the specified password.
+    # function to delete the specified password.
     website = input('Input the website you would like to delete: ')
     c.execute("SELECT * FROM passwords WHERE website = ?", (website,))
     result = c.fetchone()
@@ -106,24 +104,25 @@ def delete_password():
 
 
 def password_strength(password):
-    # checks the strength of the inputted password
+    # function to check the strength of the inputted password
     if len(password) < 5:
         print('Your password is too weak.')
         abort_process()
-    special_chars = "!@#$%^&*()_-+={}[]|\:;'<>,./?"
+    special_chars = [i for i in "!@#$%^&*()_-+={}[]|\:;'<>,./?"]
 
-    for char in password:
-        if char in special_chars:
-            return
+    for char in special_chars:
+        if char in password:
+            break
         else:
             print('Your password must contain at least 1 special character')
             abort_process()
 
 
 def generate_password(length):
-    special_chars = (random.choice(["!@#$%^&*()_+{}:\"<>?,./;'[]"]) for i in range(length))
-    letters_lower = (random.choice(string.ascii_lowercase) for i in range(length))
-    letters_upper = (random.choice(string.ascii_uppercase) for i in range(length))
+    # generates a strong password
+    special_chars = (random.choice(["!@#$%^&*()_+{}:\"<>?,./;'[]\\"]) for _ in range(length))
+    letters_lower = (random.choice(string.ascii_lowercase) for _ in range(length))
+    letters_upper = (random.choice(string.ascii_uppercase) for _ in range(length))
     result_str = ''.join(letters_lower) + ''.join(letters_upper) + ''.join(special_chars)
     shuffle = random.sample(result_str, len(result_str))
     return ''.join(shuffle)
@@ -143,13 +142,16 @@ while True:
     os.system('cls')
     match choice:
         case '1':
+            # Show all passwords
             show_passwords()
             input('Press any key to continue: ')
             os.system('cls')
         case '2':
+            # Search for a specific password
             query = input("Enter search: ")
             search_passwords(query)
         case '3':
+            # Add a new password
             psd = input('Enter your password or enter 1 to auto generate: ')
             if psd == '1':
                 psd = generate_password(4)
@@ -157,16 +159,20 @@ while True:
             add_password(psd)
             os.system('cls')
         case '4':
+            # Update a password
             psd = input('Enter your password or enter 1 to auto generate: ')
             if psd == '1':
                 psd = generate_password(4)
             password_strength(psd)
             update_password(psd)
         case '5':
+            # Delete a password
             delete_password()
         case '6':
-            break
+            # Abort the process
+            abort_process()
         case _:
+            # anything else inputted
             print("Invalid choice. Please try again.")
             time.sleep(1)
             os.system('cls')
